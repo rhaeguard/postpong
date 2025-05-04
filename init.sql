df22f33612beea7ff9e0af78ad26a8a2a65a6874
Mansur <mensurowary@gmail.com>
2025-05-04 03:36:42 -0400

    feat: split into client/server + 2 players

---
+++ init.sql
@@ -0,0 +1,312 @@
+drop table if exists user_inputs;
+
+create table user_inputs(
+        game_id int primary key,
+        -- player a props
+		player_a_id int,
+		player_a_move int,
+		player_a_x real, -- user properties
+		player_a_y real,
+		player_a_active bool,
+        -- player b props
+		player_b_id int,
+        player_b_move int,
+		player_b_x real, -- user properties
+		player_b_y real,
+		player_b_active bool,
+        -- game props
+		b_x real, -- ball properties
+		b_y real,
+		b_r real,
+		b_vx real, -- ball velocity properties
+		b_vy real
+);
+
+drop table if exists flat_table;
+create table flat_table(
+		id int primary key,
+		screen_x real,
+		screen_y real,
+		screen_w real,
+		screen_h real,
+		top_x real,
+		top_y real,
+		top_w real,
+		top_h real,
+		bottom_x real,
+		bottom_y real,
+		bottom_w real,
+		bottom_h real,
+		racket_width real,
+        racket_thickness real
+);
+
+create or replace function get_state()
+   returns table (
+		ox real,
+		oy real
+   )
+   language plpgsql
+  as
+$$
+declare
+   ox real;
+   oy real;
+begin
+	select x, y into ox, oy from user_inputs;
+	return query select ox, oy;
+end;
+$$;
+
+create or replace function check_collision_circle_rec(
+		cx real,
+		cy real,
+		cr real,
+		rx real,
+		ry real,
+		rw real,
+		rh real
+)
+   returns bool
+   language plpgsql
+  as
+$$
+declare
+	collision bool;
+	recCenterX real;
+	recCenterY real;
+	dx real;
+	dy real;
+	corderDistSq real;
+begin
+		recCenterX := rx + rw/2.0;
+		recCenterY := ry + rh/2.0;
+
+		dx := abs(cx - recCenterX);
+		dy := abs(cy - recCenterY);
+
+		if dx > (rw/2.0) + cr then
+			return false;
+		end if;
+
+		if dy > (rh/2.0) + cr then
+			return false;
+		end if;
+
+		if dx <= (rw/2.0) then
+			return true;
+		end if;
+
+		if dy <= (rh/2.0) then
+			return true;
+		end if;
+
+		corderDistSq := (dx - (rw/2.0))*(dx - (rw/2.0)) + (dy - (rh/2.0))*(dy - (rh/2.0)); 
+
+		collision := corderDistSq <= cr*cr;
+
+	return collision;
+end;
+$$;
+
+create or replace function update()
+   returns int
+   language plpgsql
+  as
+$$
+declare
+   -- player a
+   p_a_ix real default -1;
+   p_a_iy real default -1;
+   p_a_ox real default -1;
+   p_a_oy real default -1;
+   p_a_move int;
+   p_a_active bool;
+   -- player b
+   p_b_ix real default -1;
+   p_b_iy real default -1;
+   p_b_ox real default -1;
+   p_b_oy real default -1;
+   p_b_move int;
+   p_b_active bool;
+   -- other stuff
+   racketWidth real default -1;
+   racketThickness real default -1;
+   racketPositionY real;
+   screenHeight real default -1;
+   -- ball
+   bx real;
+   by_ real;
+   br real;
+   bvx real;
+   bvy real;
+   -- borders
+   i_bottom_x real;
+   i_bottom_y real;
+   i_bottom_w real;
+   i_bottom_h real;
+   i_top_x real;
+   i_top_y real;
+   i_top_w real;
+   i_top_h real;
+   -- checks
+   top_check bool;
+   bottom_check bool;
+   right_check bool;
+   p_a_racket_check bool;
+   p_b_racket_check bool;
+begin
+	-- should we begin the game?
+	select
+		player_a_active, player_b_active
+	into
+		p_a_active, p_b_active
+	from user_inputs where game_id=1;
+
+	if not p_a_active and not p_b_active then
+		return 2;
+	end if;
+
+	select 
+        -- player a
+		player_a_x, player_a_y, player_a_move,
+        -- player b
+		player_b_x, player_b_y, player_b_move,
+        -- ball fields
+        b_x, b_y, b_r, b_vx, b_vy
+    into 
+		p_a_ix, p_a_iy, p_a_move, -- player a
+		p_b_ix, p_b_iy, p_b_move, -- player b
+        bx, by_, br, bvx, bvy     -- ball
+	from user_inputs where game_id=1;
+	
+	select 
+		screen_h, racket_width, racket_thickness
+		into screenHeight, racketWidth, racketThickness
+	from flat_table where id=1;
+
+    -- player a
+	if p_a_move = 1 then
+		p_a_oy := p_a_iy - 10; 
+	elsif p_a_move = 2 then
+		p_a_oy := p_a_iy + 10;
+	else
+		p_a_ox := p_a_ix;
+		p_a_oy := p_a_iy;
+	end if;
+    
+	if p_a_oy - racketWidth/2 < 0 then
+		p_a_oy := p_a_iy;
+	elsif p_a_oy + racketWidth/2 > screenHeight then
+		p_a_oy := p_a_iy;
+	end if;
+    -- player a end
+
+    -- player b start
+	if p_b_move = 1 then
+		p_b_oy := p_b_iy - 10; 
+	elsif p_b_move = 2 then
+		p_b_oy := p_b_iy + 10;
+	else
+		p_b_ox := p_b_ix;
+		p_b_oy := p_b_iy;
+	end if;
+
+    if p_b_oy - racketWidth/2 < 0 then
+		p_b_oy := p_b_iy;
+	elsif p_b_oy + racketWidth/2 > screenHeight then
+		p_b_oy := p_b_iy;
+	end if;
+    -- player b end
+
+	update user_inputs 
+    set 
+        player_a_x = p_a_ix, player_a_y = p_a_oy, 
+        player_b_x = p_b_ix, player_b_y = p_b_oy 
+    where game_id=1; -- update the user positions
+
+	-- update the ball starts
+	bx := bx + bvx;
+	by_ := by_ + bvy;
+
+	select 
+		bottom_x, bottom_y, bottom_w, bottom_h,
+		top_x, top_y, top_w, top_h
+	into
+		i_bottom_x, i_bottom_y, i_bottom_w, i_bottom_h,
+		i_top_x, i_top_y, i_top_w, i_top_h
+	from flat_table where id=1;
+
+	bottom_check := check_collision_circle_rec(
+		bx,
+		by_,
+		br, 
+		i_bottom_x, 
+		i_bottom_y, 
+		i_bottom_w, 
+		i_bottom_h
+	);
+
+	top_check := check_collision_circle_rec(
+		bx,
+		by_,
+		br, 
+		i_top_x, 
+		i_top_y, 
+		i_top_w, 
+		i_top_h
+	);
+
+	right_check := check_collision_circle_rec(
+		bx,
+		by_,
+		br, 
+		i_top_x+i_top_w, 
+		i_top_y, 
+		i_top_h, 
+		i_top_w
+	);
+
+    -- racket checks
+	p_a_racket_check := check_collision_circle_rec(
+		bx,
+		by_,
+		br, 
+		p_a_ox,
+		CAST((p_a_oy - racketWidth/2) as real), 
+		racketThickness, 
+		racketWidth
+	);
+
+    p_b_racket_check := check_collision_circle_rec(
+		bx,
+		by_,
+		br, 
+		p_b_ox - racketThickness,
+		CAST((p_b_oy - racketWidth/2) as real), 
+		racketThickness, 
+		racketWidth
+	);
+
+	if bottom_check then
+		update user_inputs set b_vy = (-bvy) where game_id=1;
+	end if;
+
+	if top_check then
+		update user_inputs set b_vy = (-bvy) where game_id=1;
+	end if;
+
+	if right_check then
+		update user_inputs set b_vx = (-bvx) where game_id=1;
+	end if;
+
+	if p_a_racket_check or p_b_racket_check then
+		update user_inputs set b_vx = (-bvx) where game_id=1;
+	end if;
+	
+	update user_inputs set b_x = bx, b_y = by_ where game_id=1;
+	-- -- update the ball ends
+
+	return 1;
+end;
+$$;
--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"context"
 	"database/sql"
+	"flag"
+	"fmt"
 	"log"
+	"os"
+	"strings"
+	"time"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 	_ "github.com/lib/pq"
@@ -15,371 +20,254 @@ const (
 	ScreenHeight = 720
 )
 
-func exec(conn *sql.Conn, functionCreationSql string, args ...any) {
+func exec(conn *sql.Conn, sql string, args ...any) {
 	if _, err := conn.ExecContext(
 		context.Background(),
-		functionCreationSql, args...,
+		sql, args...,
 	); err != nil {
-		log.Fatalf("(%s)\n%s\n", functionCreationSql, err)
+		log.Fatalf("(%s)\n%s\n", sql, err)
 	} else {
 	}
 }
 
-func ticker(conn *sql.Conn) {
-	rows, err := conn.QueryContext(context.Background(), `SELECT update();`)
-	if err != nil {
-		panic(err.Error())
-	}
-	rows.Close()
+type ExecPair struct {
+	name  string
+	value any
 }
 
-func main() {
-	screen := rl.NewRectangle(
+func ep(name string, value any) ExecPair {
+	return ExecPair{
+		name, value,
+	}
+}
+
+func execWithNamedArgs(conn *sql.Conn, sql string, args ...ExecPair) {
+	sqlPlaceholders := []string{}
+	sqlArgs := []any{}
+	for i, pair := range args {
+		sqlPlaceholders = append(sqlPlaceholders, fmt.Sprintf("$%d", i+1))
+		sqlArgs = append(sqlArgs, pair.value)
+	}
+	placeholder := strings.Join(sqlPlaceholders, ",")
+	sql = strings.Replace(sql, "...", placeholder, 1)
+
+	if _, err := conn.ExecContext(context.Background(), sql, sqlArgs...); err != nil {
+		log.Fatalf("(%s)\n%s\n", sql, err)
+	} else {
+	}
+}
+
+var screen rl.Rectangle
+var playableBorder rl.Rectangle
+var top rl.Rectangle
+var bottom rl.Rectangle
+var racketWidth float32
+var racketThickness float32
+
+func initialize() {
+	screen = rl.NewRectangle(
 		0, 0, float32(ScreenWidth), float32(ScreenHeight),
 	)
-	playableBorder := rl.NewRectangle(
+	playableBorder = rl.NewRectangle(
 		GAP, GAP, screen.Width-(2*GAP), screen.Height-(2*GAP),
 	)
 
-	top := rl.NewRectangle(
+	top = rl.NewRectangle(
 		playableBorder.X, screen.Y, playableBorder.Width, playableBorder.Y,
 	)
 
-	bottom := rl.NewRectangle(
+	bottom = rl.NewRectangle(
 		playableBorder.X, playableBorder.Y+playableBorder.Height, playableBorder.Width, playableBorder.Y,
 	)
-	racketWidth := float32(200)
+	racketWidth = float32(200)
+	racketThickness = GAP
+}
 
-	// database stuff
-	db, _ := sql.Open("postgres", "postgres://postgres:mysecretpassword@127.0.0.1:5432/postgres?sslmode=disable")
-	if err := db.Ping(); err != nil {
-		log.Fatalf("%s\n", err)
-	}
-	defer db.Close()
+func initializeDB(conn *sql.Conn) {
+	bytes, err := os.ReadFile("init.sql")
 
-	conn, err := db.Conn(context.Background())
 	if err != nil {
-		log.Fatalf("%s\n", err)
-	}
-	defer conn.Close()
-
-	{ // init code
-		functionCreationSql := `
-drop table if exists user_inputs;
-create table user_inputs(
-		player_id int,
-		move int,
-		x real, -- user properties
-		y real,
-		b_x real, -- ball properties
-		b_y real,
-		b_r real,
-		b_vx real, -- ball velocity properties
-		b_vy real
-);
-
-drop table if exists flat_table;
-create table flat_table(
-		id int,
-		screen_x real,
-		screen_y real,
-		screen_w real,
-		screen_h real,
-		top_x real,
-		top_y real,
-		top_w real,
-		top_h real,
-		bottom_x real,
-		bottom_y real,
-		bottom_w real,
-		bottom_h real,
-		racket_width real
-);
-
-create or replace function get_state()
-   returns table (
-		ox real,
-		oy real
-   )
-   language plpgsql
-  as
-$$
-declare
-   ox real;
-   oy real;
-begin
-	select x, y into ox, oy from user_inputs;
-	return query select ox, oy;
-end;
-$$;
-
-create or replace function check_collision_circle_rec(
-		cx real,
-		cy real,
-		cr real,
-		rx real,
-		ry real,
-		rw real,
-		rh real
-)
-   returns bool
-   language plpgsql
-  as
-$$
-declare
-	collision bool;
-	recCenterX real;
-	recCenterY real;
-	dx real;
-	dy real;
-	corderDistSq real;
-begin
-		recCenterX := rx + rw/2.0;
-		recCenterY := ry + rh/2.0;
-
-		dx := abs(cx - recCenterX);
-		dy := abs(cy - recCenterY);
-
-		if dx > (rw/2.0) + cr then
-			return false;
-		end if;
-
-		if dy > (rh/2.0) + cr then
-			return false;
-		end if;
-
-		if dx <= (rw/2.0) then
-			return true;
-		end if;
-
-		if dy <= (rh/2.0) then
-			return true;
-		end if;
-
-		corderDistSq := (dx - (rw/2.0))*(dx - (rw/2.0)) + (dy - (rh/2.0))*(dy - (rh/2.0)); 
-
-		collision := corderDistSq <= cr*cr;
-
-	return collision;
-end;
-$$;
-
-create or replace function update()
-   returns int
-   language plpgsql
-  as
-$$
-declare
-   ix real default -1;
-   iy real default -1;
-   ox real default -1;
-   oy real default -1;
-   racketWidth real default -1;
-   racketPositionY real;
-   screenHeight real default -1;
-   user_move int;
-   bx real;
-   by_ real;
-   br real;
-   bvx real;
-   bvy real;
-   i_bottom_x real;
-   i_bottom_y real;
-   i_bottom_w real;
-   i_bottom_h real;
-   bottom_check bool;
-   i_top_x real;
-   i_top_y real;
-   i_top_w real;
-   i_top_h real;
-   top_check bool;
-   right_check bool;
-   racket_check bool;
-begin
-	select 
-		x, y, b_x, b_y, b_r, b_vx, b_vy,
-		move
-		into 
-		ix, iy, bx, by_, br, bvx, bvy,
-		user_move 
-	from user_inputs where player_id=1;
-	
-	select 
-		screen_h, racket_width 
-		into screenHeight, racketWidth 
-	from flat_table where id=1;
-
-	if user_move = 1 then
-		oy := iy - 10; 
-	elsif user_move = 2 then
-		oy := iy + 10;
-	else
-		ox := ix;
-		oy := iy;
-	end if;
-
-	if oy - racketWidth/2 < 0 then
-		oy := iy;
-	elsif oy + racketWidth/2 > screenHeight then
-		oy := iy;
-	end if;
-
-	update user_inputs set x = ix, y = oy where player_id=1;
-
-	-- update the ball starts
-	bx := bx + bvx;
-	by_ := by_ + bvy;
-
-	select 
-		bottom_x, bottom_y, bottom_w, bottom_h,
-		top_x, top_y, top_w, top_h
-	into
-		i_bottom_x, i_bottom_y, i_bottom_w, i_bottom_h,
-		i_top_x, i_top_y, i_top_w, i_top_h
-	from flat_table where id=1;
-
-	bottom_check := check_collision_circle_rec(
-		bx,
-		by_,
-		br, 
-		i_bottom_x, 
-		i_bottom_y, 
-		i_bottom_w, 
-		i_bottom_h
-	);
-
-	top_check := check_collision_circle_rec(
-		bx,
-		by_,
-		br, 
-		i_top_x, 
-		i_top_y, 
-		i_top_w, 
-		i_top_h
-	);
-
-	right_check := check_collision_circle_rec(
-		bx,
-		by_,
-		br, 
-		i_top_x+i_top_w, 
-		i_top_y, 
-		i_top_h, 
-		i_top_w
-	);
-
-	racketPositionY := oy - racketWidth/2;
-
-	racket_check := check_collision_circle_rec(
-		bx,
-		by_,
-		br, 
-		ox, -- assuming player on the left 
-		racketPositionY, 
-		i_top_y, 
-		racketWidth
-	);
-
-	if bottom_check then
-		update user_inputs set b_vy = (-bvy) where player_id=1;
-	end if;
-
-	if top_check then
-		update user_inputs set b_vy = (-bvy) where player_id=1;
-	end if;
-
-	if right_check then
-		update user_inputs set b_vx = (-bvx) where player_id=1;
-	end if;
-
-	if racket_check then
-		update user_inputs set b_vx = (-bvx) where player_id=1;
-	end if;
-
-	
-	
-	update user_inputs set b_x = bx, b_y = by_ where player_id=1;
-	-- update the ball ends
-
-	return 1;
-end;
-$$;
-`
-		exec(conn, functionCreationSql)
-
-		exec(
-			conn, `insert into flat_table values (1,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13);`,
-			// args
-			screen.X, screen.Y, screen.Width, screen.Height,
-			top.X, top.Y, top.Width, top.Height,
-			bottom.X, bottom.Y, bottom.Width, bottom.Height,
-			racketWidth,
-		)
-
-		exec(
-			conn, `insert into user_inputs values (1, 0, $1, $2, $3, $4, $5, $6, $7);`,
-			// args
-			0.0, screen.Height/2,
-			screen.Width/2, screen.Height/2,
-			screen.Width/128,
-			10, 10,
-		)
+		log.Fatalf("Could not open the init.sql file: %s", err.Error())
 	}
 
+	functionCreationSql := string(bytes)
+
+	exec(conn, functionCreationSql)
+
+	execWithNamedArgs(
+		conn, `insert into flat_table values (...);`,
+		ep("id", 1),
+		// screen
+		ep("screen_x", screen.X),
+		ep("screen_y", screen.Y),
+		ep("screen_w", screen.Width),
+		ep("screen_h", screen.Height),
+		// top border
+		ep("top_x", top.X),
+		ep("top_y", top.Y),
+		ep("top_w", top.Width),
+		ep("top_h", top.Height),
+		// bottom border
+		ep("bottom_x", bottom.X),
+		ep("bottom_y", bottom.Y),
+		ep("bottom_w", bottom.Width),
+		ep("bottom_h", bottom.Height),
+		// racket
+		ep("racket_width", racketWidth),
+		ep("racket_thickness", racketThickness),
+	)
+
+	execWithNamedArgs(
+		conn, `insert into user_inputs values (...);`,
+		ep("game_id", 1),
+		// player a
+		ep("player_a_id", 1),
+		ep("player_a_move", 0),
+		ep("player_a_x", 0.0),
+		ep("player_a_y", screen.Height/2),
+		ep("player_a_active", false),
+		// player b
+		ep("player_b_id", 1),
+		ep("player_b_move", 0),
+		ep("player_b_x", screen.Width),
+		ep("player_b_y", screen.Height/2),
+		ep("player_b_active", false),
+		// ball position
+		ep("b_x", screen.Width/2),
+		ep("b_y", screen.Height/2),
+		ep("b_r", screen.Width/128),
+		ep("b_vx", 5), // TODO: ball speed?
+		ep("b_vy", 5),
+	)
+}
+
+func update(conn *sql.Conn) {
+	const TickRate = 60
+
+	ticker := time.NewTicker(time.Second / TickRate)
+
+	for range ticker.C {
+		exec(conn, `SELECT update();`) // 16.67 ms hard limit
+	}
+}
+
+type Player struct {
+	id    int
+	move  int
+	x     float32
+	y     float32
+	color rl.Color
+}
+
+func gameplay(conn *sql.Conn) {
 	rl.SetTraceLogLevel(rl.LogError)
-	rl.InitWindow(int32(screen.Width), int32(screen.Height), "bingbong")
+	rl.InitWindow(int32(screen.Width), int32(screen.Height), "postpong")
 	defer rl.CloseWindow()
 
 	rl.SetTargetFPS(60)
 
-	var x float32
-	var y float32
-
 	// ball properties
 	var bx, by, br float32
 
+	total := float32(0.0)
+	totalCount := 0
+
+	var playerA = Player{
+		id:   1,
+		move: 0,
+		x:    0.0,
+		y:    0.0,
+	}
+
+	var playerB = Player{
+		id:   1,
+		move: 0,
+		x:    0.0,
+		y:    0.0,
+	}
+
+	exec(conn, `UPDATE user_inputs SET player_a_active=TRUE WHERE player_a_id=$1 AND game_id=1;`, playerA.id)
+	exec(conn, `UPDATE user_inputs SET player_b_active=TRUE WHERE player_b_id=$1 AND game_id=1;`, playerB.id)
+
 	for !rl.WindowShouldClose() {
+		start := time.Now()
 		{
-			move := 0
-			// grab user input section
-			if rl.IsKeyDown(rl.KeyUp) {
-				move = 1
+			{
+				newMove := 0
+				// grab user input section
+				if rl.IsKeyDown(rl.KeyW) {
+					newMove = 1
+				}
+
+				if rl.IsKeyDown(rl.KeyS) {
+					newMove = 2
+				}
+
+				if playerA.move != newMove {
+					exec(conn, `UPDATE user_inputs SET player_a_move=$1 WHERE player_a_id=$2 AND game_id=1;`, newMove, playerA.id)
+					playerA.move = newMove
+				}
 			}
 
-			if rl.IsKeyDown(rl.KeyDown) {
-				move = 2
-			}
+			{
+				newMove := 0
+				// grab user input section
+				if rl.IsKeyDown(rl.KeyUp) {
+					newMove = 1
+				}
 
-			exec(conn, `UPDATE user_inputs SET move=$1 WHERE player_id=1;`, move)
+				if rl.IsKeyDown(rl.KeyDown) {
+					newMove = 2
+				}
+
+				if playerB.move != newMove {
+					exec(conn, `UPDATE user_inputs SET player_b_move=$1 WHERE player_b_id=$2 AND game_id=1;`, newMove, playerB.id)
+					playerB.move = newMove
+				}
+			}
 		}
 		{
-			// update section
-			ticker(conn)
-		}
-		{
-			rows, err := conn.QueryContext(context.Background(), `SELECT x, y, b_x, b_y, b_r from user_inputs;`)
+			rows, err := conn.QueryContext(
+				context.Background(),
+				`SELECT 
+				player_a_x, player_a_y, 
+				player_b_x, player_b_y, 
+				b_x, b_y, b_r 
+				from user_inputs 
+				where game_id=1;`,
+			)
 			if err != nil {
 				panic(err.Error())
 			}
 
 			for rows.Next() {
-				if err := rows.Scan(&x, &y, &bx, &by, &br); err != nil {
+				if err := rows.Scan(&playerA.x, &playerA.y, &playerB.x, &playerB.y, &bx, &by, &br); err != nil {
 					panic(err.Error())
 				}
 			}
 
 			rows.Close()
 		}
+		total += float32(time.Since(start))
+		totalCount++
 
 		rl.BeginDrawing()
 
 		rl.ClearBackground(rl.RayWhite)
 
+		// draw player A
 		rl.DrawRectangleV(
-			rl.NewVector2(screen.X, y-racketWidth/2),
+			rl.NewVector2(screen.X, playerA.y-racketWidth/2),
 			rl.NewVector2(GAP, racketWidth),
 			rl.Blue,
 		)
+		// draw player B
+		rl.DrawRectangleV(
+			rl.NewVector2(screen.Width-GAP, playerB.y-racketWidth/2),
+			rl.NewVector2(GAP, racketWidth),
+			rl.DarkGreen,
+		)
+		// draw ball
 		rl.DrawCircleV(
 			rl.NewVector2(bx, by),
 			br,
@@ -396,5 +284,58 @@ $$;
 		rl.DrawFPS(0, 0)
 
 		rl.EndDrawing()
+	}
+
+	exec(conn, `UPDATE user_inputs SET player_a_active=FALSE WHERE player_a_id=$1 AND game_id=1;`, playerA.id)
+	exec(conn, `UPDATE user_inputs SET player_b_active=FALSE WHERE player_b_id=$1 AND game_id=1;`, playerB.id)
+
+	avg := (total / float32(totalCount)) / 1e6
+	fmt.Printf("Avg Update Time: %.4f\n", avg)
+}
+
+func GetEnvOrDefault(varname string, defaultValue string) string {
+	if value, ok := os.LookupEnv(varname); ok {
+		return value
+	}
+	return defaultValue
+}
+
+func main() {
+	isServer := flag.Bool("server", false, "is this binary the server or the game client")
+	flag.Parse()
+
+	var pgUsername string = GetEnvOrDefault("PONG_PG_USERNAME", "postgres")
+	var pgPasswd string = GetEnvOrDefault("PONG_PG_PASSWORD", "mysecretpassword")
+	var pgNetloc string = GetEnvOrDefault("PONG_PG_NETLOC", "127.0.0.1")
+	var pgPort string = GetEnvOrDefault("PONG_PG_PORT", "5432")
+	var pgDb string = GetEnvOrDefault("PONG_PG_DB", "postgres")
+
+	connectionString := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		pgUsername, pgPasswd, pgNetloc, pgPort, pgDb,
+	)
+
+	// database stuff
+	db, _ := sql.Open("postgres", connectionString)
+	if err := db.Ping(); err != nil {
+		log.Fatalf("%s\n", err)
+	}
+	defer db.Close()
+
+	conn, err := db.Conn(context.Background())
+	if err != nil {
+		log.Fatalf("%s\n", err)
+	}
+	defer conn.Close()
+
+	initialize()
+
+	if *isServer {
+		fmt.Println("Server is running...")
+		initializeDB(conn)
+		update(conn)
+	} else {
+		fmt.Println("Game is running...")
+		gameplay(conn)
 	}
 }
